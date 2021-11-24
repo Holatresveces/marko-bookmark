fd4591ca5cc213bb8a6cfaeeb79f8cf7cd34b58b
Lalo <eduardo.j.a@ciencias.unam.mx>
2021-11-24 06:33:12 -0600

    Save bookmarks to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,20 @@ import { Bookmark } from "./interfaces";
 import BookmarkList from "./components/BookmarkList";
 import AddBookmarkDialog from "./components/AddBookmarkDialog";
 
-const initialBookmarks: Bookmark[] = [
-  {
-    url: "https://bedu.org",
-    title: "Cursos de Programación, Tecnología, Negocios, Inglés | BEDU",
-    description:
-      "Somos una compañía de educación en línea en español que ofrece cursos de programación, cursos de negocios y cursos de inglés en un esquema acelerado y flexible.",
-    image: "http://assets.bedu.org/images/hero-image.jpg",
-  },
-  {
-    url: "https://www.youtube.com",
-    title: "YouTube",
-    description:
-      "Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube.",
-    image: "http://www.youtube.com/img/desktop/yt_1200.png",
-  },
-  {
-    url: "https://www.instagram.com",
-    title: "Instagram",
-    description:
-      "Create an account or log in to Instagram - A simple, fun & creative way to capture, edit & share photos, videos & messages with friends & family.",
-    image: null,
-  },
-];
-
 const App = () => {
-  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>(() => {
+    const value = window.localStorage.getItem("bookmarks");
+    if (value) {
+      return JSON.parse(value);
+    } else {
+      return [];
+    }
+  });
   const [showAddBookmarkDialog, setShowAddBookmarkDialog] = useState(false);
 
   useEffect(() => {
-    setBookmarks(initialBookmarks);
-  }, []);
+    window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }, [bookmarks]);
 
   const toggleAddBookmarkDialog = () => {
     setShowAddBookmarkDialog(!showAddBookmarkDialog);

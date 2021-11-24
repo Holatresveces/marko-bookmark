e6cab5644bef998b1d78328c7dcf5ff15098997b
Lalo <eduardo.j.a@ciencias.unam.mx>
2021-11-24 09:32:09 -0600

    Add Header component styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,29 @@
+import { IoIosArrowDown } from "react-icons/io";
 import profilePicture from "../images/profile-icon.jpg";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Marko</h1>
-      <div>
-        <div>My bookmarks</div>
-        <div>
-          <div>
-            <img src={profilePicture} alt="" />
+    <header className="border-b border-gray-200">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center p-5">
+          <h1 className="font-bold text-xl">Marko</h1>
+          <div className="flex items-center font-Inter">
+            <div className="font-semibold">My bookmarks</div>
+            <div className="flex items-center ml-16">
+              <div className="w-10 h-10 rounded-full overflow-hidden mr-3">
+                <img src={profilePicture} alt="" />
+              </div>
+              <div>
+                <div className="text-xs font-normal text-gray-400">Welcome</div>
+                <div className="font-medium">Johana Doe</div>
+              </div>
+              <div className="w-10 h-10 flex items-center justify-center">
+                <span className="cursor-pointer">
+                  <IoIosArrowDown />
+                </span>
+              </div>
+            </div>
           </div>
-          <div>
-            <div>Welcome</div>
-            <div>Johana Doe</div>
-          </div>
-          <div>V</div>
         </div>
       </div>
     </header>

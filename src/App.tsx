3f614bb5d265da66e036059cef1b72569d226d8a
Lalo <eduardo.j.a@ciencias.unam.mx>
2021-11-24 23:12:43 -0600

    Refactor App.tsx structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,41 @@
-import { useEffect, useState } from "react";
-import { Bookmark } from "./interfaces";
-import BookmarkList from "./components/BookmarkList";
-import AddBookmarkDialog from "./components/AddBookmarkDialog";
+import { useBookmarks } from "./hooks/useBookmarks";
+import { useState } from "react";
 import Header from "./components/Header";
-import Button from "./components/Button";
 import Welcome from "./components/Welcome";
+import BookmarksContainer from "./components/BookmarksContainer";
+import AddBookmarkDialog from "./components/AddBookmarkDialog";
 
 const App = () => {
-  const [bookmarks, setBookmarks] = useState<Bookmark[]>(() => {
-    const value = window.localStorage.getItem("bookmarks");
-    if (value) {
-      return JSON.parse(value);
-    } else {
-      return [];
-    }
-  });
+  const { bookmarks, addBookmark, deleteBookmark } = useBookmarks();
   const [showAddBookmarkDialog, setShowAddBookmarkDialog] = useState(false);
-
-  useEffect(() => {
-    window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
-    setShowAddBookmarkDialog(false);
-  }, [bookmarks]);
 
   const toggleAddBookmarkDialog = () => {
     setShowAddBookmarkDialog(!showAddBookmarkDialog);
   };
 
-  const addBookmark = (bookmark: Bookmark) => {
-    setBookmarks([...bookmarks, bookmark]);
-  };
-
-  const deleteBookmark = ({ url }: Bookmark) => {
-    const filteredBookmarks = bookmarks.filter(
-      (bookmark) => bookmark.url !== url
-    );
-    setBookmarks(filteredBookmarks);
-  };
-
   return (
     <>
       <Header />
-      <main className="bg-gray-200 font-Inter h-screen">
-        <div className="max-w-7xl mx-auto">
-          <div className="p-5">
-            {bookmarks.length === 0 ? (
-              <Welcome toggleAddBookmarkDialog={toggleAddBookmarkDialog} />
-            ) : (
-              <>
-                <div className="flex justify-between items-center mt-14 mb-9">
-                  <div>
-                    <h1 className="font-semibold text-2xl mb-2">
-                      My Bookmarks
-                    </h1>
-                    <div className="w-12 h-1 bg-indigo-600"></div>
-                  </div>
-                  <Button
-                    onClick={toggleAddBookmarkDialog}
-                    text="Add new bookmark"
-                  />
-                </div>
 
-                <BookmarkList
-                  bookmarks={bookmarks}
-                  deleteBookmark={deleteBookmark}
-                />
-              </>
-            )}
-            {showAddBookmarkDialog && (
-              <AddBookmarkDialog
-                addBookmark={addBookmark}
-                bookmarks={bookmarks}
-                toggleDialog={toggleAddBookmarkDialog}
-              />
-            )}
-          </div>
-        </div>
-      </main>
+      {bookmarks.length === 0 ? (
+        <Welcome toggleAddBookmarkDialog={toggleAddBookmarkDialog} />
+      ) : (
+        <BookmarksContainer
+          addBookmark={addBookmark}
+          bookmarks={bookmarks}
+          deleteBookmark={deleteBookmark}
+          showAddBookmarkDialog={showAddBookmarkDialog}
+          toggleAddBookmarkDialog={toggleAddBookmarkDialog}
+        />
+      )}
+
+      {showAddBookmarkDialog && (
+        <AddBookmarkDialog
+          addBookmark={addBookmark}
+          bookmarks={bookmarks}
+          toggleDialog={toggleAddBookmarkDialog}
+        />
+      )}
     </>
   );
 };

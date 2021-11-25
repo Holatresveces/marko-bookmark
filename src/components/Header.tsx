78566a97b81f0c1c0674095d7e3e950b45eaacd8
Lalo <eduardo.j.a@ciencias.unam.mx>
2021-11-25 00:57:59 -0600

    Refactor Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import NavProfileCard from "./NavProfileCard";
 
 const Header = () => {
   return (
-    <header className="border-b border-gray-200">
+    <header className="absolute w-full bg-white border-b border-gray-200">
       <div className="max-w-7xl mx-auto flex justify-between items-center p-5">
         <h1 className="font-bold text-xl">Marko</h1>
         <div className="flex items-center font-Inter">

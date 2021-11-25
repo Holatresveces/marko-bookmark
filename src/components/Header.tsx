c223fec665255f24dc620ec7ab07a0be1b76a963
Lalo <eduardo.j.a@ciencias.unam.mx>
2021-11-24 23:18:01 -0600

    Refactor Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,13 @@
-import { IoIosArrowDown } from "react-icons/io";
-import profilePicture from "../images/profile-icon.jpg";
+import NavProfileCard from "./NavProfileCard";
 
 const Header = () => {
   return (
     <header className="border-b border-gray-200">
-      <div className="max-w-7xl mx-auto">
-        <div className="flex justify-between items-center p-5">
-          <h1 className="font-bold text-xl">Marko</h1>
-          <div className="flex items-center font-Inter">
-            <div className="font-semibold">My bookmarks</div>
-            <div className="flex items-center ml-16">
-              <div className="w-10 h-10 rounded-full overflow-hidden mr-3">
-                <img src={profilePicture} alt="" />
-              </div>
-              <div>
-                <div className="text-xs font-normal text-gray-400">Welcome</div>
-                <div className="font-medium">Johana Doe</div>
-              </div>
-              <div className="w-10 h-10 flex items-center justify-center">
-                <span className="cursor-pointer">
-                  <IoIosArrowDown />
-                </span>
-              </div>
-            </div>
-          </div>
+      <div className="max-w-7xl mx-auto flex justify-between items-center p-5">
+        <h1 className="font-bold text-xl">Marko</h1>
+        <div className="flex items-center font-Inter">
+          <div className="font-semibold">My bookmarks</div>
+          <NavProfileCard />
         </div>
       </div>
     </header>
